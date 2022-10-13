1280e57842dda6f6bd954317945318cdcbca018b
lackylucky <57640295+lackylucky@users.noreply.github.com>
2022-10-13 23:54:56 +0200

    Update incoming_ocr.sh

--- incoming_ocr.sh
+++ incoming_ocr.sh
@@ -2,25 +2,23 @@
 source=/input/
 output=/output/
 maximumsize=5240
-echo "Wait for File"
-inotifywait -m -e create -e moved_to --format "%f" $source \
-        | while read FILENAME
-                do
-                        echo "New File detected"
-                        ocrtime=$(date +%Y%m%d-%H%M%S)
-                        if [ "${FILENAME: -4}" == ".pdf" ]; then
-                                echo "New file is a pdf"
-                                actualsize=$(du -k "$f" | cut -f 1)
-                                if [ $actualsize -ge $maximumsize ]; then
-                                        echo size is over $maximumsize kilobytes, no ocr
-                                        mv $FILENAME $output/$ocrtime'_'$FILENAME
-                                else
-                                        echo "OCR the new file"
-                                        ocrmypdf $source/$FILENAME $output/$ocrtime'_'$FILENAME
-                                        rm $source/$FILENAME
-                                fi
-                        else
-                                echo "New file is not a pdf, no ocr"
-                                mv mv $FILENAME $output/$ocrtime'_'$FILENAME
-                        fi
-                  done
+
+for f in *; do
+        echo $f
+        ocrtime=$(date +%Y%m%d-%H%M%S)
+        if [ "${f: -4}" == ".pdf" ]; then
+                echo File $f is a pdf
+                actualsize=$(du -k "$f" | cut -f 1)
+                if [ $actualsize -ge $maximumsize ]; then
+                        echo $f is $actualsize kilobytes and over the maximum of $maximumsize kilobytes, sorry no ocr
+                        mv $f $output/$ocrtime'_'$f
+                else
+                        echo $f is $actualsize kilobytes, lets OCR
+                        ocrmypdf $source/$f $output/$ocrtime'_'$f
+                        rm $source/$f
+                fi
+        else
+                echo File $f is not a pdf, no ocr
+                mv mv $f $output/$ocrtime'_'$f
+        fi
+done
