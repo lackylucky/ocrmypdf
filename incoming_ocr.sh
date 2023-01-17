7e4fd43b7ef2016621c3ffef7a47e051abf74550
lackylucky <57640295+lackylucky@users.noreply.github.com>
2023-01-17 23:08:08 +0100

    Update incoming_ocr.sh

--- incoming_ocr.sh
+++ incoming_ocr.sh
@@ -3,24 +3,24 @@ source=/input/
 output=/output/
 maximumsize=5240
 
-inotifywait -r -m "$source" --format '%w%f' -e CREATE,MOVED_TO | while IFS= read -r item
+inotifywait -r -m "$source" --format '%w%f' -e CREATE,MOVED_TO | while read path action file
 do      
         cd $source
-        echo $item
+        echo "The file '$file' appeared in directory '$path' via '$action'"
         ocrtime=$(date +%Y%m%d-%H%M%S)
-        if [ "${item: -4}" == ".pdf" ]; then
-                echo File $item is a pdf
-                actualsize=$(du -k "$item" | cut -f 1)
+        if [ "${file: -4}" == ".pdf" ]; then
+                echo File $file is a pdf
+                actualsize=$(du -k "$file" | cut -f 1)
                 if [ $actualsize -ge $maximumsize ]; then
-                        echo $item is $actualsize kilobytes and over the maximum of $maximumsize kilobytes, sorry no ocr
-                        mv $item $output/$ocrtime'_'$item
+                        echo $file is $actualsize kilobytes and over the maximum of $maximumsize kilobytes, sorry no ocr
+                        mv $file $output/$ocrtime'_'$file
                 else
-                        echo $item is $actualsize kilobytes, lets OCR
-                        ocrmypdf $source/$item $output/$ocrtime'_'$item
-                        rm $source/$item
+                        echo $file is $actualsize kilobytes, lets OCR
+                        ocrmypdf $source/$file $output/$ocrtime'_'$file
+                        rm $source/$file
                 fi
         else
-                echo File $item is not a pdf, no ocr
-                mv mv $item $output/$ocrtime'_'$item
+                echo File $file is not a pdf, no ocr
+                mv mv $file $output/$ocrtime'_'$file
         fi
 done
