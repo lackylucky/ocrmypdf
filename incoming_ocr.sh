7c55bef7aa8bf7438190bb98c4b12929273669b6
lackylucky <57640295+lackylucky@users.noreply.github.com>
2023-01-24 11:26:30 +0100

    Update incoming_ocr.sh

--- incoming_ocr.sh
+++ incoming_ocr.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
-source=/input/
-output=/output/
-maximumsize=5240
+source=/input
+target=/output
 
-inotifywait -r -m "$source" -e CREATE,MOVED_TO | while read path action file
+if [ $LANG != "engl" ]; then
+        Echo Install $LANG
+        apk add --no-cache tesseract-ocr-data-$LANG
+fi
+
+inotifywait -r -m "$source/$INPUT" -e CREATE,MOVED_TO | while read path action file
 do      
-        cd $source
+        cd $source/$INPUT
         echo "The file '$file' appeared in directory '$path' via '$action'"
         ocrtime=$(date +%Y%m%d-%H%M%S)
         if [ "${file: -4}" == ".pdf" ]; then
                 echo File $file is a pdf
                 actualsize=$(du -k "$file" | cut -f 1)
-                if [ $actualsize -ge $maximumsize ]; then
-                        echo $file is $actualsize kilobytes and over the maximum of $maximumsize kilobytes, sorry no ocr
-                        mv $file $output/$ocrtime'_'$file
+                if [ $actualsize -ge $MAXSIZE ]; then
+                        echo $file is $actualsize kilobytes and over the maximum of $MAXSIZE kilobytes, sorry no ocr
+                        mv $file $target/$OUTPUT/$ocrtime'_'$file
                 else
                         echo $file is $actualsize kilobytes, lets OCR
-                        ocrmypdf $source/$file $output/$ocrtime'_'$file
+                        ocrmypdf -d -c -l $LANG $source/$INPUT/$file $target/$OUTPUT/$ocrtime'_'$file
                         echo "OCR done, remove file '$file' in '$path'"
-                        rm $source/$file
+                        rm $source/$INPUT/$file
                         echo "file removed"
                 fi
         else
                 echo File $file is not a pdf, no ocr
-                mv $file $output/$ocrtime'_'$file
-                echo "File moved to output folder"
+                mv $file $target/$OUTPUT/$ocrtime'_'$file
+                echo "File moved to $target/$OUTPUT"
         fi
 done
