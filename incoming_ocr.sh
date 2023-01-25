a5494bbed90248edd8da26f5c61f7b404d41c697
lackylucky <57640295+lackylucky@users.noreply.github.com>
2023-01-25 21:31:21 +0100

    Update incoming_ocr.sh

--- incoming_ocr.sh
+++ incoming_ocr.sh
@@ -10,28 +10,28 @@ else
 	echo Install $language
         apk add --no-cache tesseract-ocr-data-$language
 fi
-#Check for new files
-inotifywait -r -m "$source/$INPUT" -e CREATE,MOVED_TO | while read path action file
-do
-        cd $source/$INPUT
-        echo "The file '$file' appeared in directory '$path' via '$action'"
+
+#Check for files
+cd $source/$INPUT
+for f in *; do
+        echo "The file '$f' appeared in directory '$source/$INPUT'"
         ocrtime=$(date +%Y%m%d-%H%M%S)
-        if [ "${file: -4}" == ".pdf" ]; then
-                echo File $file is a pdf
-                actualsize=$(du -k "$file" | cut -f 1)
+        if [ "${f: -4}" == ".pdf" ]; then
+                echo File $f is a pdf
+                actualsize=$(du -k "$f" | cut -f 1)
                 if [[ $actualsize -ge $MAXSIZE ]]; then
-                        echo $file is $actualsize kilobytes and over the maximum of $MAXSIZE kilobytes, sorry no ocr
-                        mv $file $target/$OUTPUT/$ocrtime'_'$file
+                        echo $f is $actualsize kilobytes and over the maximum of $MAXSIZE kilobytes, sorry no ocr
+                        mv $f $target/$OUTPUT/$ocrtime'_'$f
                 else
-                        echo $file is $actualsize kilobytes, lets OCR
-                        ocrmypdf -d -c -l $language $source/$INPUT/$file $target/$OUTPUT/$ocrtime'_'$file
-                        echo "OCR finished, remove file '$file' in '$path'"
-                        rm $source/$INPUT/$file
+                        echo $f is $actualsize kilobytes, lets OCR
+                        ocrmypdf -d -c -l $language $source/$INPUT/$f $target/$OUTPUT/$ocrtime'_'$f
+                        echo "OCR finished, remove file '$f' in '$source/$INPUT'"
+                        rm $source/$INPUT/$f
                         echo "file removed"
                 fi
         else
-                echo File $file is not a pdf, no ocr
-                mv $file $target/$OUTPUT/$ocrtime'_'$file
+                echo File $f is not a pdf, no ocr
+                mv $f $target/$OUTPUT/$ocrtime'_'$f
                 echo "File moved to $target/$OUTPUT"
         fi
 done
