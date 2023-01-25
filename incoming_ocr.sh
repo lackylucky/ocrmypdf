2b2a79819af7324830b540a908115ef7f5a21f82
lackylucky <57640295+lackylucky@users.noreply.github.com>
2023-01-25 23:06:20 +0100

    Update incoming_ocr.sh

--- incoming_ocr.sh
+++ incoming_ocr.sh
@@ -5,26 +5,28 @@ language="$(echo $LANG | tr [:upper:] [:lower:])"
 
 #Check for files
 cd $source/$INPUT
-for f in *.*; do
-        echo "The file '$f' appeared in directory '$source/$INPUT'"
-        ocrtime=$(date +%Y%m%d-%H%M%S)
-        if [ "${f: -4}" == ".pdf" ]; then
-                echo File $f is a pdf
-                actualsize=$(du -k "$f" | cut -f 1)
-                if [[ $actualsize -ge $MAXSIZE ]]; then
-                        echo $f is $actualsize kilobytes and over the maximum of $MAXSIZE kilobytes, sorry no ocr
-                        mv $f $target/$OUTPUT/$ocrtime'_'$f
+for f in *; do
+        if [ ! -d "$f" ]; then
+                echo "The file '$f' appeared in directory '$source/$INPUT'"
+                ocrtime=$(date +%Y%m%d-%H%M%S)
+                if [ "${f: -4}" == ".pdf" ]; then
+                        echo File $f is a pdf
+                        actualsize=$(du -k "$f" | cut -f 1)
+                        if [[ $actualsize -ge $MAXSIZE ]]; then
+                                echo $f is $actualsize kilobytes and over the maximum of $MAXSIZE kilobytes, sorry no ocr
+                                mv $f $target/$OUTPUT/$ocrtime'_'$f
+                        else
+                               echo $f is $actualsize kilobytes, lets OCR
+                                ocrmypdf -d -c -l $language $source/$INPUT/$f $target/$OUTPUT/$ocrtime'_'$f
+                               echo "OCR finished, remove file '$f' in '$source/$INPUT'"
+                               rm $source/$INPUT/$f
+                              echo "file removed"
+                        fi
                 else
-                        echo $f is $actualsize kilobytes, lets OCR
-                        ocrmypdf -d -c -l $language $source/$INPUT/$f $target/$OUTPUT/$ocrtime'_'$f
-                        echo "OCR finished, remove file '$f' in '$source/$INPUT'"
-                        rm $source/$INPUT/$f
-                        echo "file removed"
+                      echo File $f is not a pdf, no ocr
+                     mv $f $target/$OUTPUT/$ocrtime'_'$f
+                       echo "File moved to $target/$OUTPUT"
                 fi
-        else
-                echo File $f is not a pdf, no ocr
-                mv $f $target/$OUTPUT/$ocrtime'_'$f
-                echo "File moved to $target/$OUTPUT"
         fi
 done
 
